1adcb0bbf7c1980b914cef6ab67a59e88a89a9fd
neeru0713 <neerurani1307@gmail.com>
2025-06-22 19:12:45 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='text-green-800'>
+     
+       
+      <h1>Vite + React</h1>
+      </div>
+  
+  )
+}
+
+export default App
