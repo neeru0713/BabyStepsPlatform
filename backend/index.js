9cb38191778e602b685943fa66f238c6a2953032
neeru0713 <neerurani1307@gmail.com>
2025-06-22 19:25:50 +0530

    backend initial commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const userRoutes = require("./routes/userRoutes.js");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get("/", (req,res) => {
+  res.send("Backend server is running")
+})
+// app.use("/", userRoutes);
+
+
+mongoose
+  .connect(
+    process.env.MONGODB_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+ 
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
