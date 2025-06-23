6badecbae83c6c5abf74265a85f12f5fc09e692c
neeru0713 <neerurani1307@gmail.com>
2025-06-23 23:55:01 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,9 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://babystepsplatform.onrender.com", credentials: true })
+);
 app.use(bodyParser.json());
 app.use(express.json());
 
